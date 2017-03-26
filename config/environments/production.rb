41c7d0a872ff018b926fb97249c1ba20cc882509
Brandon Wayne Odiwuor <brandon.odiwuor@gmail.com>
2017-03-26 13:49:27 +0000

    Update smtp sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   #SendGrid SMTP email configuration
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'factorout.herokuapp.com'
+  host = 'thefactorout.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
